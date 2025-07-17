8e9b11d20283c8db1a7d99854883426a53706da0
Nathan Lilienthal <nathan@nixpulvis.com>
2025-07-16 21:15:23 -0400

    Use clap for CLI

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,69 +1,73 @@
-use std::env;
+use clap::{Parser, Subcommand};
 use tokio;
-use valet::db::{DEFAULT_URL, Database, Lots, Users};
+use valet::prelude::*;
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    // TODO: Remove password from CLI, it should be prompted.
+    Validate {
+        username: String,
+        password: String,
+    },
+    Register {
+        username: String,
+        password: String,
+    },
+
+    // TODO: Move rest of commands into baked in REPL under unlock subcommand.
+    // Unlock {
+    //     username: String,
+    // },
+    Put {
+        username: String,
+        password: String,
+        data: String,
+    },
+    Get {
+        username: String,
+        password: String,
+    },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), valet::db::Error> {
-    let mut args = env::args();
+    let cli = Cli::parse();
+    let db = Database::new(valet::db::DEFAULT_URL).await?;
 
-    // TODO: Import a proper CLI arg parser.
-    if let Some(command) = args.nth(1) {
-        match command.as_str() {
-            "register" => {
-                let username = args.nth(0);
-                let password = args.nth(0);
-                if let (Some(username), Some(password)) = (username, password) {
-                    let db = Database::new(DEFAULT_URL).await?;
-                    Users::register(&db, &username, &password).await?;
-                } else {
-                    println!("No username/password given.")
-                }
-            }
-            "validate" => {
-                let username = args.nth(0);
-                let password = args.nth(0);
-                if let (Some(username), Some(password)) = (username, password) {
-                    let db = Database::new(DEFAULT_URL).await?;
-                    let user = Users::get(&db, &username, &password).await?;
-                    println!("{} validated", user.username);
-                }
-            }
-            "put" => {
-                let username = args.nth(0);
-                let password = args.nth(0);
-                let data = args.nth(0);
-                if let (Some(username), Some(password), Some(data)) = (username, password, data) {
-                    let db = Database::new(DEFAULT_URL).await?;
-                    let user = Users::get(&db, &username, &password).await?;
-                    let encrypted = user
-                        .key()
-                        .encrypt(data.as_bytes())
-                        .expect("failed to encrypt");
-                    Lots::create(&db, &user.username, &encrypted).await?;
-                } else {
-                    println!("No username/password/data given.")
-                }
-            }
-            "get" => {
-                let username = args.nth(0);
-                let password = args.nth(0);
-                if let (Some(username), Some(password)) = (username, password) {
-                    let db = Database::new(DEFAULT_URL).await?;
-                    let user = Users::get(&db, &username, &password).await?;
-                    let encrypted = Lots::get(&db, &user.username).await?;
-                    let bytes = user.key().decrypt(&encrypted).expect("failed to decrypt");
-                    let data = std::str::from_utf8(&bytes).expect("failed to parse data");
-                    println!("{}", data);
-                } else {
-                    println!("No username/password given.")
-                }
-            }
-            _ => {
-                println!("Unknown command.")
-            }
+    match &cli.command {
+        Command::Validate { username, password } => {
+            let user = valet::db::Users::get(&db, &username, &password).await?;
+            println!("{} validated", user.username);
         }
-    } else {
-        println!("No command given.")
+        Command::Register { username, password } => {
+            let user = valet::db::Users::register(&db, &username, &password).await?;
+            println!("{} registered", user.username);
+        }
+        Command::Put {
+            username,
+            password,
+            data,
+        } => {
+            let user = valet::db::Users::get(&db, &username, &password).await?;
+            let encrypted = user
+                .key()
+                .encrypt(data.as_bytes())
+                .expect("failed to encrypt");
+            valet::db::Lots::create(&db, &user.username, &encrypted).await?;
+        }
+        Command::Get { username, password } => {
+            let user = valet::db::Users::get(&db, &username, &password).await?;
+            let encrypted = valet::db::Lots::get(&db, &user.username).await?;
+            let bytes = user.key().decrypt(&encrypted).expect("failed to decrypt");
+            let data = std::str::from_utf8(&bytes).expect("failed to parse data");
+            println!("{}", data);
+        }
     }
 
     Ok(())
