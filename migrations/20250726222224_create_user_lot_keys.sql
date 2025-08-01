e54a17a5e6a01ca0620c6319b25937dbf1dde965
Nathan Lilienthal <nathan@nixpulvis.com>
2025-08-01 06:45:22 -0400

    Temporarily remove user_lot_keys

--- migrations/20250726222224_create_user_lot_keys.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_lot_keys (
-    username   TEXT  NOT NULL,
-    lot_uuid   TEXT  NOT NULL,
-    key_data   BLOB  NOT NULL,
-    key_nonce  BLOB  NOT NULL,
-    PRIMARY KEY (username, lot_uuid),
-    FOREIGN KEY (username) REFERENCES users (username),
-    FOREIGN KEY (lot_uuid) REFERENCES lots (uuid)
-);
