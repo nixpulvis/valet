adc9a08c30bb7292786310e74dc3aca3be08326d
nixpulvis <nixpulvis@users.noreply.github.com>
2026-03-03 15:17:27 +0000

    deploy: 4240ec60f4cba0030b29b394b42f81305de85756

--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"valet/encrypt/struct.PasswordBuf.html\" title=\"struct valet::encrypt::PasswordBuf\">PasswordBuf</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"valet/uuid/struct.Uuid.html\" title=\"struct valet::uuid::Uuid\">Uuid</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"valet/encrypt/struct.PasswordBuf.html\" title=\"struct valet::encrypt::PasswordBuf\">PasswordBuf</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"valet/uuid/struct.Uuid.html\" title=\"struct valet::uuid::Uuid\">Uuid</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"valet/encrypt/struct.PasswordBuf.html\" title=\"struct valet::encrypt::PasswordBuf\">PasswordBuf</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"valet/encrypt/struct.Key.html\" title=\"struct valet::encrypt::Key\">Key</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"valet/encrypt/struct.PasswordBuf.html\" title=\"struct valet::encrypt::PasswordBuf\">PasswordBuf</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"valet/encrypt/struct.Key.html\" title=\"struct valet::encrypt::Key\">Key</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
