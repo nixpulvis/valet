ea13b101038343655d8e5515fc97b857297f4d8e
nixpulvis <nixpulvis@users.noreply.github.com>
2026-04-20 03:03:57 +0000

    deploy: 086825422d31cf1fa49c682077f2bc9abe8b5aa9

--- valet/record/sidebar-items.js
+++ valet/record/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error","LabelName"],"mod":["query"],"struct":["Data","Label","Record","RecordIndex"]};
+window.SIDEBAR_ITEMS = {"enum":["Error","LabelName"],"mod":["query"],"struct":["Data","Label","Record","RecordIndex","Revision"]};
