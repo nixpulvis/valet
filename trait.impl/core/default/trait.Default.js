58d09fff5ee6e4edbcb5646f3eb1696077a50c40
nixpulvis <nixpulvis@users.noreply.github.com>
2026-04-22 03:36:30 +0000

    deploy: d55d93c54b8c03391d610a474fdc68bb2ee30030

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"valet/password/struct.Password.html\" title=\"struct valet::password::Password\">Password</a>",0]]]]);
+    const implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"valet/password/struct.Password.html\" title=\"struct valet::password::Password\">Password</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"valet/record/struct.RecordIndex.html\" title=\"struct valet::record::RecordIndex\">RecordIndex</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[291]}
+//{"start":59,"fragment_lengths":[576]}
---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["valet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"valet/encrypt/enum.Error.html\" title=\"enum valet::encrypt::Error\">Error</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"valet/uuid/enum.Error.html\" title=\"enum valet::uuid::Error\">Error</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[511]}
